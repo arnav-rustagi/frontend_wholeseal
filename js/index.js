50895fe3e75484eb5298120c395c6f45af0c6de9
Arnav <arnav.rus237@gmail.com>
2022-02-07 12:49:34 +0530

    added some javascript

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var a,b,c ;
+function hoverEffect(a){
+    a.classList.add("shadow-sm");
+}
+function hoverEffectRemoval(b){
+    b.classList.remove("shadow-sm");
+}
+
+function imgHoverEffect(c){
+    c.classList.add("hoverZoom");
+}
+function imgHoverEffectRemoval(c){
+    c.classList.remove("hoverZoom");
+}
